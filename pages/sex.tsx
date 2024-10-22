1744476815e7d96cbc3121de3dd787b25324ee42
Yushirizu <113351099+Yushirizu@users.noreply.github.com>
2024-10-22 19:48:20 +0200

    POP IN

--- pages/sex.tsx
+++ pages/sex.tsx
@@ -23,7 +23,7 @@ export default function Sex() {
                 width: "100%",
                 height: "100%",
               }}
-              src="https://www.youtube.com/embed/WbhL7DHSJ1A?si=xSTOL9I4dnlq0YfF"
+              src="https://www.youtube.com/embed/c7E-tgmFuzw?si=h9VZjqF0lQtXRxtF"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               title="YouTube video"
