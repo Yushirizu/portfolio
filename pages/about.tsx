c9032eef9ad10edea15c65b1c30d842150e9c796
Yushirizu <113351099+Yushirizu@users.noreply.github.com>
2025-01-05 20:32:35 +0100

    feat: expand about page with strengths and weaknesses section

--- pages/about.tsx
+++ pages/about.tsx
@@ -170,6 +170,122 @@ export default function About() {
           ways that engage and entertain his streams with such simple ideas yet
           so impactful while making his AI feel more interactive and human.
         </Typography>
+        <Typography
+          variant="h2"
+          mt={5}
+          sx={{ fontWeight: "bold", textAlign: "center" }}
+        >
+          Strength and weaknesses
+        </Typography>
+        <Typography variant="h5" mt={2} sx={{ fontWeight: "bold" }}>
+          Competitive
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          You might have seen in my projects that I participated in two
+          competitions (the hackathon and WorldSkills) and I will not stop here,
+          a new hackathon was announced, and I will also participate. But why do
+          I love competition?
+        </Typography>
+        <Typography
+          mt={2}
+          mb={5}
+          sx={{ textIndent: "2em", textAlign: "justify" }}
+        >
+          It is for the “fight” against a person, pushing yourself and pushing
+          other people or a single person to their limits and see who will make
+          the best program, or have the best system. For example, with a friend
+          will always compete and see who gets the best marks on lessons it
+          pushes us to study and win against the other. So, for me, it is also a
+          motivation.
+        </Typography>
+        <Typography variant="h5" mt={2} sx={{ fontWeight: "bold" }}>
+          Perseverance
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          As you might have already seen my hobbies, I talk about some games
+          that need perseverance to succeed such as the game “Geometry Dash” but
+          here it is also in the project. For example, in the 3rd-year project,
+          I did not give up saying that it would be impossible to achieve in
+          time, I worked hard trying to finish it. I do not know if I will make
+          it in time though, but I will do everything to finish it.
+        </Typography>
+        <Typography variant="h5" mt={2} sx={{ fontWeight: "bold" }}>
+          Autonomous
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          I love learning new things by myself or working by myself, for
+          instance when at the University of Mons, I started programming video
+          games using Unity and now that is why I had an advantage against the
+          others in terms of Unity knowledge for the industrial project. Another
+          example is that in my 2nd-year multidisciplinary project, I talked
+          about how I worked alone during the Easter holidays, I programmed the
+          Arduino code for the camera during that time, even if it was the
+          holidays. I generally do not need someone to tell me something to do
+          it.
+        </Typography>
+        <Typography variant="h5" mt={2} sx={{ fontWeight: "bold" }}>
+          Imposter syndrome
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          When I am looking for a job/internship offer I do not know if I am
+          qualified enough for that job, or I say to myself that thousands of
+          people more qualified than me would apply and get that job. I feel
+          like a lot of friends around me who are in IT are also having this,
+          the feel of not being qualified enough or not sure about their skills.
+          I think it is because it is pretty much impossible to put on a scale
+          the level of skill required for a job in programming.
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          For example, if a company asked me to do computer visualisation. I
+          already worked a little on the subject with my project Lenny and the
+          industrial project but how much should I know because computer
+          visualisation is very vast? Should I build my AI? So, I should know
+          all the maths and the algorithms behind but then I should learn about
+          Large Language Models (LLMs) and neural networks and go on and on
+          thinking that it would be impossible for me. The job might just want
+          you to do very high-level programming where you would only use a
+          library and that’s all.
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          In conclusion, it is hard to find where my skills are and how they can
+          be shown to other people in the industry.
+        </Typography>
+        <Typography variant="h5" mt={2} sx={{ fontWeight: "bold" }}>
+          Strong-minded
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          I always stray to have the last word during an argument or I always to
+          be right. The only thing that could change my point of view is
+          concrete arguments on the question. For example, when I was working on
+          the 2nd-year automation project, I was sure of myself for the PID
+          variables (see the text on the 2nd-year automation project) but then a
+          member of my group told me that those variables were wrong, and I
+          could not believe I was sure of myself. He had to turn on the system
+          with his variables and for me to see the results on the graph to see
+          that he was right in the end.
+        </Typography>
+        <Typography variant="h5" mt={2} sx={{ fontWeight: "bold" }}>
+          Disorganisation
+        </Typography>
+        <Typography mt={2} sx={{ textIndent: "2em", textAlign: "justify" }}>
+          Whether it is in my tasks, my project or my pc’s folders there will be
+          little to no organisation. I find it difficult to organize and
+          structure things in such a way that could make sense for other people.
+          I often catch myself having way too many folders on my pc and cannot
+          find a file or having so many tasks on a project that I cannot think
+          where to begin. In general, in that case, I just work and see where I
+          go next. But sometimes I do organise my work like in the hackathon
+          where I had a good overview of the tasks to do and in what order.
+        </Typography>
+        <Typography
+          mt={2}
+          mb={5}
+          sx={{ textIndent: "2em", textAlign: "justify" }}
+        >
+          In conclusion, in most of my projects, I tend to be quite disorganized
+          and work on multiple projects or tasks at the same time. However, in
+          the end, I still manage to come to an end.
+        </Typography>
       </Container>
     </>
   );
